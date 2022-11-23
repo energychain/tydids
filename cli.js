500fe5a7d63729f36ed23346421651d652dc82ff
Thorsten Zoerner <me@thorsten-zoerner.com>
2022-11-23 17:10:50 +0100

    1.0.2

--- cli.js
+++ cli.js
@@ -1,18 +1,13 @@
 const lib = require("./lib");
 const { ethers } = require("ethers");
 
-
-
 const app = async function() {
     const wallet = lib.wallet();
-    console.log(wallet.privateKey);
-    let signature = await wallet.tydids.signMessage({test:"TATE"});
-    console.log(wallet.address);
-    console.log(signature);
-    const verify = wallet.tydids.verifyMessage({test:"TATE"},signature);
-    console.log(verify);
-    console.log(wallet.tydids.hashMessage("TEST"));
-    console.log("ND.");
+    const hash = '0x54476e19525abcebdbe3fd61c3eaa8399d7f449f37f786a188a190c3cb2786cf';
+    let signature = await wallet.tydids.signMessage(hash);
+    console.log('Signature',signature);
+    console.log('Owner',wallet.address);
+    console.log('Hash',hash)
 }
 
 app();
